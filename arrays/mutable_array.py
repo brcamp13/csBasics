912a4faf7c5ebddaf0a63b63ac2654bb1ad32914
brcamp13 <brandon.camp.bell@outlook.com>
2019-08-11 21:46:13 -0700

    Finish manual implementation of mutable array

--- arrays/mutable_array.py
+++ arrays/mutable_array.py
@@ -17,22 +17,50 @@ class MutableArray:
         return self.size == 0
 
     def at(self, index):
-        if index > capacity-1:
+        if index > self.capacity-1:
             print("Index out of range error")
             return -1
         return self.array[index]
 
     def push(self, item):
         if self.size < self.capacity:
-            self.array[size] = item
-            size += 1
+            self.array[self.size] = item
+            self.size += 1
         else:
-            new_array = [None] * (self.capacity * 2)
-            for value in self.array:
-                new_array.append(value)
-            new_array.append(item)
-            size += 1
+            self.resize(self.capacity*2)
+            self.size += 1
             self.capacity *= 2
 
     def insert(self, index, item):
-        
+        for i in range(len(self.array)-1, index, -1):
+            temp = self.array[i]
+            self.array[i+1] = temp
+        self.array[index] = item
+
+    def prepend(self, item):
+        self.insert(0, item)
+
+    def pop(self):
+        item = self.array[-1]
+        self.array.remove()
+        return item
+
+    def delete(self, index):
+        for i in range(index, len(self.array) - 1):
+            self.array[index] = self.array[i+1]
+
+    def remove(self, item):
+        for i in range(0, len(self.array)):
+            if self.array[i] == item: 
+                self.delete(i)
+
+    def find(self, item):
+        for i in range(0, len(self.array)):
+            if self.array[i] == item:
+                return i
+        return -1
+
+    def resize(self, new_capacity):
+        new_array = [None] * (new_capacity)
+        for value in self.array:
+            new_array.append(value)
